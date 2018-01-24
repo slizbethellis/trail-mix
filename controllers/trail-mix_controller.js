f837cee83d0a53a08abe8c32feb2614879acc58e
Lonnie Laub <llaub01@gmail.com>
2018-01-23 19:45:51 -0600

    file save stuff

--- controllers/trail-mix_controller.js
+++ controllers/trail-mix_controller.js
@@ -46,8 +46,39 @@ router.get("/game", function(req, res) {
 });
 
 router.post("/admin", function(req, res) {
-
   var newAdv = req.body;
+
+  var customer = db.Customers.findall({
+    where: {
+      customer_id: req.params.id
+    }
+  });
+  var custName = customer.cust_name;
+  var custFolder = "../customer-images/" + custName;
+
+  var cust_logo = req.files.cust_logo;
+  var adventure_image1 = req.files.adventure_image1;
+  var adventure_image2 = req.files.adventure_image2;
+  var adventure_image3 = req.files.adventure_image3;
+
+  // move and rename
+  cust_logo.mv(custFolder + "/" + custName + "Logo.jpg", function(err) {
+  if (err)
+    return res.status(500).send(err);
+  });
+  adventure_image1.mv(custFolder + "/" + newAdv.adventure_name + "-image1.jpg", function(err) {
+  if (err)
+    return res.status(500).send(err);
+  });
+  adventure_image2.mv(custFolder + "/" + newAdv.adventure_name + "-image2.jpg", function(err) {
+  if (err)
+    return res.status(500).send(err);
+  });
+  adventure_image3.mv(custFolder + "/" + newAdv.adventure_name + "-image3.jpg", function(err) {
+  if (err)
+    return res.status(500).send(err);
+  });
+
   db.Adventures.create({
     // this might work with just a simple 'req.body'
     adventure_name: newAdv.adventure_name,
@@ -68,7 +99,7 @@ router.post("/admin", function(req, res) {
     };
     res.render("admin", hbObject);
   })
->>>>>>> 14f76526dd9abdba460f14c0fac0b406eb9733d0
+
 });
 
 module.exports = router;
