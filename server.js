7e34dfdd929e94272af89837558d9a01320ffbf3
slizbethellis <slizbethellis@gmail.com>
2018-01-24 08:25:03 -0600

    more okta tweaks

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 var exphbs = require("express-handlebars");
+const url = require('url');
 const fileUpload = require('express-fileupload');
 const session = require('express-session');
 const { ExpressOIDC } = require('@okta/oidc-middleware');
@@ -14,16 +15,17 @@ const oidc = new ExpressOIDC({
     issuer: 'https://dev-851045-admin.oktapreview.com/oauth2/default',
     client_id: '0oadr0d9mxDyWPDCt0h7',
     client_secret: 'qgvteql3tVXdUYwWi4aDCnYy1qwDNTTqPs-L3F5_',
-    redirect_uri:  'https://shrouded-beach-16284.herokuapp.com/authorization-code/callback' /*|| 'http://localhost:8080/authorization-code/callback'*/,
+    redirect_uri:  /*'https://shrouded-beach-16284.herokuapp.com/authorization-code/callback' ||*/ 'http://localhost:8080/authorization-code/callback',
     scope: 'openid profile email',
     routes: {
         login: {
             viewHandler: (req, res) => {
+                const baseUrl = url.parse('https://dev-851045-admin.oktapreview.com').protocol + '//' + url.parse('https://dev-851045-admin.oktapreview.com').host;
                 // Render your custom login page, you must create this view for your application and use the Okta Sign-In Widget
                 res.render('custom-login', {
                     csrfToken: req.csrfToken(),
                     myClientId: '0oadr0d9mxDyWPDCt0h7',
-                    baseUrl: 'https://dev-851045-admin.oktapreview.com/oauth2/default'
+                    baseUrl: baseUrl
                 });
             }
         }
@@ -79,5 +81,3 @@ oidc.on('ready', () => {
 oidc.on('error', err => {
     console.log('Unable to configure ExpressOIDC', err);
 });
-
- oidc;
