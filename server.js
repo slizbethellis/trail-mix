9f3ce78c5d5d1acf3f0f25b537884735ede84e65
slizbethellis <slizbethellis@gmail.com>
2018-01-24 07:48:43 -0600

    trying to get other auth providers working in okta

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+var exphbs = require("express-handlebars");
 const fileUpload = require('express-fileupload');
 const session = require('express-session');
 const { ExpressOIDC } = require('@okta/oidc-middleware');
@@ -11,10 +12,22 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 const oidc = new ExpressOIDC({
     issuer: 'https://dev-851045-admin.oktapreview.com/oauth2/default',
-    client_id: '0oado5o8navAe4p630h7',
-    client_secret: '2SjkVo-ZrYEN5X-yt0KkIqyOaPVwtRhrM1z9uXvV',
+    client_id: '0oadr0d9mxDyWPDCt0h7',
+    client_secret: 'qgvteql3tVXdUYwWi4aDCnYy1qwDNTTqPs-L3F5_',
     redirect_uri:  'https://shrouded-beach-16284.herokuapp.com/authorization-code/callback' /*|| 'http://localhost:8080/authorization-code/callback'*/,
-    scope: 'openid profile email'
+    scope: 'openid profile email',
+    routes: {
+        login: {
+            viewHandler: (req, res) => {
+                // Render your custom login page, you must create this view for your application and use the Okta Sign-In Widget
+                res.render('custom-login', {
+                    csrfToken: req.csrfToken(),
+                    myClientId: '0oadr0d9mxDyWPDCt0h7',
+                    baseUrl: 'https://dev-851045-admin.oktapreview.com/oauth2/default'
+                });
+            }
+        }
+    }
 });
 
 var db = require("./models");
@@ -29,8 +42,6 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.use(express.static(path.join(__dirname + '/public')));
 
 // Set Handlebars.
-var exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
@@ -41,14 +52,13 @@ app.use(session({
     saveUninitialized: false
 }));
 
+// ExpressOIDC will attach handlers for the /login and /authorization-code/callback routes
+app.use(oidc.router);
+
 // Routes
 var routes = require("./controllers/trail-mix_controller.js");
 
 app.use("/", routes);
-//require("./routes/html-routes.js")(app);
-
-// ExpressOIDC will attach handlers for the /login and /authorization-code/callback routes
-app.use(oidc.router);
 
 app.get('/logout', (req, res) => {
     req.logout();
@@ -70,4 +80,4 @@ oidc.on('error', err => {
     console.log('Unable to configure ExpressOIDC', err);
 });
 
-module.exports = oidc;
+ oidc;
