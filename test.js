923a21330825d0e3a26a870190b94f6ad563c1a7
Jennifer Bono <jennb24@gmail.com>
2018-01-27 10:09:13 -0600

    test page

--- test.js
+++ test.js
@@ -1,6 +1,29 @@
-var expect = require('chai').expect;
+var Nightmare = require("nightmare");
+var expect = require("chai").expect;
 
-var game = require("./public/assets/js/game.js");
+describe("Trail-mix", function() {
+  // The default tests in mocha is 2 seconds.
+  // Extending it to 30 seconds to have time to load the pages
 
-expect(storyLine).to.be.a('array');
-expect(storyLine).to.have.lengthOf(6);
+  this.timeout(30000);
+  it("should send user to the home page", function(done) {
+    // ID for the login button.
+    Nightmare({ show: true })
+      .goto("https://shrouded-beach-16284.herokuapp.com/")
+      // Click the catalog link
+      .click("a[href='/']")
+      // Evaluate the title
+      .evaluate(function() {
+        return document.title;
+      })
+      // Asset the title is as expected
+      .then(function(title) {
+        expect(title).to.equal("Trail Mix");
+        done();
+      });
+  });
+
+  it("should throw an error for fun", function() {
+    throw new Error("Failed on purpose, just to make the Mocha output more interesting.");
+  });
+});
